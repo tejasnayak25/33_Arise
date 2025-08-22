0b5118101c32c378c9a0987bf79dea9523fd4382
Tejas Nayak <arpithanayak900@gmail.com>
2025-08-22 13:48:12 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
 
 export default nextConfig;
