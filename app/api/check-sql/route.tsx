ea79f2369802b142a8da9251ec9a371a38e7dae3
Tejas Nayak <arpithanayak900@gmail.com>
2025-08-22 06:21:46 +0530

    update

--- app/api/check-sql/route.tsx
+++ app/api/check-sql/route.tsx
@@ -7,6 +7,11 @@ export async function POST(request: NextRequest) {
     try {
         let { url } = await request.json();
         let data = await runScan(url, "sql");
+
+        if (!data) {
+            return NextResponse.json({ error: "Scan failed" }, { status: 500 });
+        }
+
         return NextResponse.json({ data });
     } catch (e) {
         return NextResponse.json({ error: e });
--- app/api/check-xss/route.tsx
+++ app/api/check-xss/route.tsx
@@ -6,8 +6,12 @@ export async function POST(request: NextRequest) {
     try {
         let { url } = await request.json();
         let data = await runScan(url, "xss");
+
+        if (!data) {
+            return NextResponse.json({ error: "Scan failed" }, { status: 500 });
+        }
+
         return NextResponse.json({ data });
-        return;
     } catch (e) {
         return NextResponse.json({ error: e });
     }
