9dade0e9c60f64334ee2f2f9fb34746f2081334f
Tejas Nayak <arpithanayak900@gmail.com>
2025-08-24 11:55:25 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
   // outputFileTracingIncludes: {
   //   'app/api/**/*': [
   //     './node_modules/@sparticuz/chromium/**/*',
